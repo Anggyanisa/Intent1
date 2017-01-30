6cc30f453ecff21edfe1247f19d25b24e20054a9
anggyanisa <anggyanisa5@gmail.com>
2017-01-30 10:14:51 +0700

    startActivity Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -2,12 +2,21 @@ package id.sch.smktelkom_mlg.learn.intent1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    findViewById(R.id.buttonHitung)
+
+    .
+
+    setOnClickListener(new View.OnClickListener()
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        {
+        }
     }
 }
